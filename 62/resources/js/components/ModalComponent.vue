6ed8bef891837018515db040867416a75d5e1648
anamitrevska <a.mitrevska@hotmail.com>
2020-09-03 22:52:31 +0200

    HW62

---
+++ 62/resources/js/components/ModalComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <!-- Button trigger modal -->
+    <slot name ="heading"></slot>
+
+    <slot name ="launch-button"></slot>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+        <slot name ="modal-header"></slot>
+        <slot name ="modal-body"></slot>
+        <slot name ="modal-footer"></slot>
+   
+          </div>
+        </div>
+      </div>
+ </div>
+
+</template>
+
+<script>
+export default {};
+</script>
